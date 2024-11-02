cdd672e94f552f658061f5c55672ae1e418d6ce2
winter000boy <sharmajidurgesh04@gmail.com>
2024-11-02 22:19:57 +0530

    SQL 50 added

---
+++ SQL 50 (Leetcode)/1378. Replace Employee ID With The Unique Identifier.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select EmployeeUNI.unique_id,employees.name from employees natural left join employeeuni
---
+++ SQL 50 (Leetcode)/584. Find Customer Referee.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select
+    name
+from
+    Customer
+where
+    referee_id != 2
+    OR
+    referee_id is null
